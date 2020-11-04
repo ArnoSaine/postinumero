5bb47792e59afef3bc58c8429924f63f526c3148
Arno Saine <arno@mowhi.com>
2020-11-04 15:41:20 +0200

    Fix using @babel/preset-env
     - Modify options set by babel-preset-react-app
     - Use merge-deep

--- packages/babel-preset-react-lib-experimental/main.js
+++ packages/babel-preset-react-lib-experimental/main.js
@@ -1,20 +1,26 @@
-const merge = require('lodash/merge');
+const merge = require('merge-deep');
+const presetEnv = require('@babel/preset-env').default;
 
 module.exports = function (api, { env, reactApp, ...opts }) {
+  const presetReactApp = require('babel-preset-react-app')(api, {
+    // For @babel/plugin-transform-runtime. Use same Babel runtime as other packages.
+    absoluteRuntime: false,
+    // For @babel/preset-react.
+    runtime: 'automatic',
+    // This can be turned on when internal imports from
+    // @babel/runtime/helpers/esm use file extensions.
+    useESModules: false,
+    ...reactApp,
+  });
+
+  const presetReactAppPresetEnvOptions = presetReactApp.presets.find(
+    ([preset]) => preset === presetEnv
+  )[1];
+
+  Object.assign(presetReactAppPresetEnvOptions, env);
+
   return merge(
     require('@postinumero/babel-preset-experimental')(api, opts),
-    require('babel-preset-react-app')(api, {
-      // For @babel/plugin-transform-runtime. Use same Babel runtime as other packages.
-      absoluteRuntime: false,
-      // For @babel/preset-react.
-      runtime: 'automatic',
-      // This can be turned on when internal imports from
-      // @babel/runtime/helpers/esm use file extensions.
-      useESModules: false,
-      ...reactApp,
-    }),
-    {
-      presets: [[require('@babel/preset-env').default, env]],
-    }
+    presetReactApp
   );
 };
