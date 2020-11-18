8c3a1af4e457f97ea5447c67cf3346e74010494d
Arno Saine <arno@mowhi.com>
2020-11-18 15:22:58 +0200

    Update dependencies

--- packages/library-scripts/src/template.js
+++ packages/library-scripts/src/template.js
@@ -8,7 +8,7 @@ import { outDir } from './build/main.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const { argv } = yargs;
+const { argv } = yargs();
 
 const type = argv.type ?? 'module';
 
