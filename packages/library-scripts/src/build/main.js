00f591133e73cb9021ade414f649bbcd323d5f9c
Arno Saine <arno@mowhi.com>
2021-08-03 23:35:07 +0300

    Cleanup

--- packages/library-scripts/src/build/main.js
+++ packages/library-scripts/src/build/main.js
@@ -11,7 +11,6 @@ const __dirname = dirname(__filename);
 export const outDirDefault = 'lib';
 
 const npmBin = spawn.sync('npm', ['bin']).stdout.toString().trim();
-const myBin = join(__dirname, '../../node_modules/.bin');
 
 // 'babel' command does not work, when developing other package and
 // libary-scripts is linked locally (babel is not added to the target
@@ -27,70 +26,70 @@ const babelBin = fs.existsSync(join(npmBin, 'babel'))
 
 const excludeArgs = ['--type'];
 
-const createBuild = ({ args, commandArgs, NODE_ENV, outDir, srcDirs }) => ({
-  outFileExtension,
-  type,
-}) => {
-  const configFile = join(
-    __dirname,
-    `babel.config.${type}${outFileExtension === '.js' ? '-js' : ''}${extname(
-      __filename
-    )}`
-  );
+const createBuild =
+  ({ args, commandArgs, NODE_ENV, outDir, srcDirs }) =>
+  ({ outFileExtension, type }) => {
+    const configFile = join(
+      __dirname,
+      `babel.config.${type}${outFileExtension === '.js' ? '-js' : ''}${extname(
+        __filename
+      )}`
+    );
 
-  const includedArgs = commandArgs.filter(
-    (arg, index, array) =>
-      !(excludeArgs.includes(arg) || excludeArgs.includes(array[index - 1]))
-  );
+    const includedArgs = commandArgs.filter(
+      (arg, index, array) =>
+        !(excludeArgs.includes(arg) || excludeArgs.includes(array[index - 1]))
+    );
 
-  spawn(
-    babelBin,
-    [
-      ...srcDirs,
-      '--out-dir',
+    spawn(
+      babelBin,
+      [
+        ...srcDirs,
+        '--out-dir',
+        outDir,
+        '--out-file-extension',
+        outFileExtension,
+        '--config-file',
+        configFile,
+        '--copy-files',
+        '--no-copy-ignored',
+        ...args.split(' '),
+        ...includedArgs,
+      ],
+      {
+        stdio: 'inherit',
+        env: {
+          NODE_ENV,
+          ...process.env,
+        },
+      }
+    );
+  };
+
+export default ({ args, NODE_ENV }) =>
+  (...commandArgs) => {
+    const { argv } = yargs(commandArgs);
+    const { outDir = outDirDefault, outFileExtension = '.js', type } = argv;
+    const srcDirs = argv._.length ? argv._ : ['src'];
+
+    const build = createBuild({
+      args,
+      commandArgs,
+      NODE_ENV,
       outDir,
-      '--out-file-extension',
-      outFileExtension,
-      '--config-file',
-      configFile,
-      '--copy-files',
-      '--no-copy-ignored',
-      ...args.split(' '),
-      ...includedArgs,
-    ],
-    {
-      stdio: 'inherit',
-      env: {
-        NODE_ENV,
-        ...process.env,
-      },
-    }
-  );
-};
+      srcDirs,
+    });
 
-export default ({ args, NODE_ENV }) => (...commandArgs) => {
-  const { argv } = yargs(commandArgs);
-  const { outDir = outDirDefault, outFileExtension = '.js', type } = argv;
-  const srcDirs = argv._.length ? argv._ : ['src'];
+    fs.removeSync(outDir);
+    fs.ensureDirSync(outDir);
 
-  const build = createBuild({
-    args,
-    commandArgs,
-    NODE_ENV,
-    outDir,
-    srcDirs,
-  });
-
-  fs.removeSync(outDir);
-  fs.ensureDirSync(outDir);
-
-  if (type) {
-    build({ outFileExtension, type });
-  } else {
-    for (const [outFileExtension, type] of options(
-      fs.readJsonSync(process.env.npm_package_json)
-    )) {
+    if (type) {
       build({ outFileExtension, type });
+    } else {
+      for (const [outFileExtension, type] of options(
+        fs.readJsonSync(process.env.npm_package_json)
+      )) {
+        build({ outFileExtension, type });
+      }
     }
-  }
-};
+  };
