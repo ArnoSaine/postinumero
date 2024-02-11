a3426cc3c690ba0805dc0fc25e49452570a03a84
Arno Saine <arno@mowhi.com>
2024-02-11 13:07:34 +0200

    Add OpenAPI Generator example

--- demos/openapi-remix-app/app/routes/_index.tsx
+++ demos/openapi-remix-app/app/routes/_index.tsx
@@ -1,4 +1,13 @@
 import type { MetaFunction } from "@remix-run/node";
+import {
+  ClientActionFunctionArgs,
+  Form,
+  json,
+  useLoaderData,
+  useNavigation,
+} from "@remix-run/react";
+import { useEffect, useRef } from "react";
+import { PetApi } from "~/api";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +16,59 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+const petApi = new PetApi();
+
+export const clientAction = async ({ request }: ClientActionFunctionArgs) => {
+  const formData = await request.formData();
+  const name = String(formData.get("name"));
+
+  await petApi.addPet({
+    pet: {
+      id: Math.floor(Math.random() * 10000 + 10000),
+      name,
+      photoUrls: [],
+      category: {
+        id: 1,
+        name: "Dogs",
+      },
+      tags: [],
+      status: "available",
+    },
+  });
+
+  return null;
+};
+
+export const clientLoader = async () => {
+  const pets = await petApi.findPetsByStatus({ status: "available" });
+
+  return json({ pets });
+};
+
 export default function Index() {
+  const { pets } = useLoaderData<typeof clientLoader>();
+
+  const navigation = useNavigation();
+  const isIdle = navigation.state === "idle";
+  const formRef = useRef<HTMLFormElement>(null);
+
+  useEffect(() => {
+    if (isIdle) {
+      formRef.current?.reset();
+    }
+  }, [isIdle]);
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Pets</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {pets.map((pet) => (
+          <li key={pet.id}>{pet.name}</li>
+        ))}
       </ul>
+      <Form method="POST" ref={formRef}>
+        <input name="name" type="text" />
+      </Form>
     </div>
   );
 }
