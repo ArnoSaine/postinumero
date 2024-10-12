607b11a65570b26f9390d81b3973eefc60bba825
Arno Saine <arno@mowhi.com>
2024-10-13 02:45:07 +0300

    Add check for `Promise.withResolvers` with error suggesting polyfill or Node.js v22

---
+++ .changeset/twelve-hornets-shake.md
@@ -0,0 +1,5 @@
+---
+"@postinumero/remix-react-intl": patch
+---
+
+Add check for `Promise.withResolvers` with error suggesting polyfill or Node.js v22
--- packages/remix-react-intl/src/remixReactIntl.ts
+++ packages/remix-react-intl/src/remixReactIntl.ts
@@ -1,7 +1,13 @@
 import { VitePluginConfig } from "@remix-run/dev";
+import invariant from "tiny-invariant";
 import remixPreset from "./remixReactIntl/remixPreset.js";
 import vitePlugin from "./remixReactIntl/vitePlugin/index.js";
 import { Opts } from "./remixReactIntl/vitePlugin/optionsPlugin.js";
+
+invariant(
+  Promise.withResolvers,
+  "Promise.withResolvers is not available. Please ensure you're using Node.js v22 or higher. If you're on an older version, you can add the necessary polyfill by importing 'core-js/proposals/promise-with-resolvers' at the top of your Vite config.",
+);
 
 export const name = "@postinumero/remix-react-intl";
 
