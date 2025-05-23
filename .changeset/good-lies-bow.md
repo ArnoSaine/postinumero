4cbdc92e8cb01f7cc0bec6d7485b71f0d888d349
Arno Saine <arno@mowhi.com>
2025-05-24 01:12:00 +0300

    Add missing types for '/app/root' and '/react-router.config.ts'

---
+++ .changeset/good-lies-bow.md
@@ -0,0 +1,5 @@
+---
+"@postinumero/react-router-formatjs": patch
+---
+
+Add missing types for '/app/root' and '/react-router.config.ts'
--- packages/react-router-formatjs/src/components/withLayoutIntlProvider.tsx
+++ packages/react-router-formatjs/src/components/withLayoutIntlProvider.tsx
@@ -1,9 +1,9 @@
 import { RawIntlProvider } from "react-intl";
-import type { RouteModule } from "../../route.d.ts";
 import { useCreateIntl } from "../intl/create.ts";
 import { useOptions } from "../options.ts";
+import type { Layout as LayoutComponent } from "/app/root";
 
-type Layout = NonNullable<RouteModule["Layout"]>;
+type Layout = NonNullable<typeof LayoutComponent>;
 
 export default function withLayoutIntlProvider(Layout: Layout): Layout {
   return function WithLayoutIntlProvider(props) {
