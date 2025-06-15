b55bc70ac7765921e741613f15ce2b3ec87047b0
Arno Saine <arno@mowhi.com>
2025-06-15 17:49:20 +0300

    Add `build.target` `ES2022` for top level await

--- packages/react-router-formatjs/examples/ssr/vite.config.ts
+++ packages/react-router-formatjs/examples/ssr/vite.config.ts
@@ -6,4 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), formatjs()],
+  build: {
+    target: "ES2022", // For top level await
+  },
 });
