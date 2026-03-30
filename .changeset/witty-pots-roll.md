bdd33da2e101dbd3c7e40f2f7103370f7cf9dcd6
Arno Saine <arno@mowhi.com>
2026-03-30 22:55:30 +0300

    Fix ErrorBoundary reset: Use navigate instead of trySigninSilent and revalidator

---
+++ .changeset/witty-pots-roll.md
@@ -0,0 +1,5 @@
+---
+"@postinumero/react-router-oidc-client": patch
+---
+
+Fix ErrorBoundary reset: Use navigate instead of trySigninSilent and revalidator
--- packages/react-router-oidc-client/src/components/TrySigninSilent.tsx
+++ packages/react-router-oidc-client/src/components/TrySigninSilent.tsx
@@ -1,16 +1,19 @@
 import { useEffect, type PropsWithChildren } from "react";
-import { useRevalidator } from "react-router";
-import { trySigninSilent } from "../user/manager.ts";
+import { useNavigate } from "react-router";
+import useLocationString from "../utils/react-router/useLocationString.ts";
 
 export default function TrySigninSilent({ children }: PropsWithChildren) {
-  const { revalidate } = useRevalidator();
+  const navigate = useNavigate();
+  const locationString = useLocationString();
 
   useEffect(() => {
     (async () => {
-      await trySigninSilent();
-      revalidate();
+      // Navigate to the root with a unique query parameter to force the ErrorBoundary reset
+      navigate("/?__rr_oidc=" + Date.now(), { replace: true });
+      await new Promise((resolve) => setTimeout(resolve));
+      navigate(locationString, { replace: true });
     })();
-  }, [revalidate]);
+  }, []);
 
   return children;
 }
